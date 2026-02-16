e488af2883a49fa822f69ff3bdcb476bfaf95d6d
Princeps13 <princeps13cs1.6@gmail.com>
2026-02-16 05:13:09 -0300

    compila

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
 
 android {
     namespace = "com.lucas.sorrentinos"
+
+    // Si te llega a dar problemas lo “release(36)”, abajo te dejo alternativa
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(36) { minorApiLevel = 1 }
     }
 
     defaultConfig {
@@ -18,7 +18,6 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -31,13 +30,13 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    buildFeatures {
-        compose = true
-    }
+
+    buildFeatures { compose = true }
 }
 
 dependencies {
@@ -46,12 +45,15 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     implementation(libs.androidx.navigation.compose)
+
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
@@ -64,3 +66,4 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+
